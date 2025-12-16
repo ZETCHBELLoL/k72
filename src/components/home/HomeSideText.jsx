d4173cd5aa232ff071000a31b615e93b4b71916a
Priyanshu Mohan <priyanshumohan1234@gmail.com>
2025-12-17 00:22:38 +0530

    homeside text corrected

---
+++ src/components/home/HomeSideText.jsx
@@ -0,0 +1,15 @@
+const HomeSideText = () => {
+  return (
+    <div className="absolute bottom-40 right-5 max-w-[280px]">
+      <p className="text-white text-sm leading-relaxed indent-20 font-[font1]">
+        K72 is an agency that builds brands from every angle. Today, tomorrow
+        and years from now. We think the best sparks fly when comfort zones
+        get left behind and friction infuses our strategies, brands and
+        communications with real feeling. We're transparent, honest and say
+        what we mean, and when we believe in something, we're all in.
+      </p>
+    </div>
+  )
+}
+
+export default HomeSideText
