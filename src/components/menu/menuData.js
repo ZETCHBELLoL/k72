3e8adb4837c7ac0026634060322608654b1f0cbd
Priyanshu Mohan <priyanshumohan1234@gmail.com>
2025-12-27 19:02:38 +0530

    Add menu marquee images

---
+++ src/components/menu/menuData.js
@@ -0,0 +1,27 @@
+// src/components/menu/menuData.js
+
+export const MENU_ITEMS = [
+  {
+    label: "WORK",
+    type: "images",
+    text: "SEE EVERYTHING",
+    images: ["1.jpg", "2.jpg"],
+  },
+  {
+    label: "AGENCY",
+    type: "images",
+    text: "KNOW US",
+    images: ["3.jpg", "4.jpg"],
+  },
+  {
+    label: "CONTACT",
+    type: "heart",
+    text: "SEND US A FAX",
+  },
+  {
+    label: "BLOG",
+    type: "images",
+    text: "READ ARTICLES",
+    images: ["5.jpg", "6.gif"],
+  },
+];
